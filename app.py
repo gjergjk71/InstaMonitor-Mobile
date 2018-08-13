9559e2da3466f9702cde63b300178e739de72226
gjergjk71 <gjergjk71@gmail.com>
2018-08-13 14:40:21 +0200

    Created LoginScreen widgets

--- app.py
+++ app.py
@@ -7,11 +7,36 @@ from kivy.uix.textinput import TextInput
 
 class LoginScreen(GridLayout):
 	def __init__(self,**kwargs):
-		super(GridLayout,self).__init__(**kwargs)
+		super(LoginScreen,self).__init__(**kwargs)
+		self.cols = 3
+		self.add_widget(Label())
+		self.add_widget(Label(text="Login",
+							  font_size=45))
+		self.add_widget(Label())
+		self.add_widget(Label(text="Username:",
+							  font_size=45))
+		self.username = TextInput(height=100,
+								  font_size=45,
+								  multiline=False)
+		self.add_widget(self.username)
+		self.add_widget(Label())
+
+		self.add_widget(Label(text="Password:",
+							  font_size=45))
+		self.password = TextInput(height=100,
+								  font_size=45,
+								  multiline=False,
+								  password=True)
+		self.add_widget(self.password)
+		self.add_widget(Label())
+
+		self.add_widget(Label())
+		self.add_widget(Button(text="Login"))
+		self.add_widget(Label())
 
 class SimpleKivy(App):
 	def build(self):
-		return LoginScreen()
+		return LoginScreen(row_force_default=True, row_default_height=70)
 
 if __name__ == "__main__":
 	SimpleKivy().run()
