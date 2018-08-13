1fb6397a2622eb3630282ba2069d4136d646208a
gjergjk71 <gjergjk71@gmail.com>
2018-08-13 14:23:07 +0200

    Defiend LoginScreen class

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+
+
+class LoginScreen(GridLayout):
+	def __init__(self,**kwargs):
+		super(GridLayout,self).__init__(**kwargs)
 
 class SimpleKivy(App):
-	pass
+	def build(self):
+		return LoginScreen()
 
 if __name__ == "__main__":
 	SimpleKivy().run()
